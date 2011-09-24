5388ffcfc68fecd6d479010beb242f416678ec9a
Katherine Scott <katherineAScott@gmail.com>
2011-09-23 23:22:39 -0400

    got coloring half done, just need to do the second pass

--- hw2/p2.cpp
+++ hw2/p2.cpp
@@ -1,7 +1,49 @@
-#include "vision_methods.h"
 #include "vision_utilities.h"
+#include "vision_methods.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+
+  for(int i=0; i < argc; i++ )
+    {
+      cout << argv[i] << endl;
+    }
+  if(argc != 3 )
+    {
+      cout << "Argument error" << endl;
+      return 1;
+    }
+  char * inname = argv[1];
+
+  Image * inImg = new Image();
+  int retVal = readImage(inImg,inname);
+  
+  if( retVal || inImg == NULL )
+    {
+      cout << "Bad input image name:" << inname << endl;
+      return retVal;
+    }
+
+  retVal =  connect_components(inImg);
+  if( retVal )
+    {
+      cout << "Failed to make connected components image." << endl;
+      return retVal; 
+    }
+
+  char * oname = argv[2];
+  retVal = writeImage(inImg,oname);
+  if( retVal )
+    {
+      cout << "Error saving file: " << oname << endl;
+      return retVal;
+    }
   return 0;
 }
+
+
--- hw2/vision_methods.cpp
+++ hw2/vision_methods.cpp
@@ -1,4 +1,9 @@
 #include "vision_methods.h"
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <iostream>
+#include "limits.h"
 using namespace std;
 //using namespace vm;
 /******************************************************************************************/ 
@@ -28,6 +33,93 @@ int threshold(Image* img, int thresh)
 int connect_components(Image* img)
 {
   int retVal = 0;
+  if(NULL == img )
+    return -1;
+  vector< set<int> > labels;
+  int w = getNCols(img);
+  int h = getNRows(img);
+  int cnt =0;
+  int c = 0;
+  int connect[4];
+  connect[0] = -1; //west
+  connect[1] = -1; //north
+  connect[2] = -1; //n.w.
+  connect[3] = -1; //n.e.
+  int clabel=-1;
+ 
+  for(int i=0;i<h;i++) // i = y
+    {
+      for(int j=0;j<w;j++) // j = x 
+	{
+	  int c = getPixel(img,i,j);
+	  if( c > 0 ) // do we have a value
+	    {
+	      if(i>0) // we can look west
+		{
+		  connect[0]=getPixel(img,i-1,j); //If we have something copy it to our location
+		}
+	      if(j > 0)
+		{
+		  connect[1] = getPixel(img,i,j-1);
+		}
+	      if( j > 0 && i > 0 )
+		{
+		  connect[2] = getPixel(img,i-1,j-1);
+		}
+	      if( j+1 < w && i > 0 )
+		{
+		  connect[3] = getPixel(img,i-1,j+1);
+		}
+	      clabel = INT_MAX;
+	      set<int> temp;
+	      for(int k=0;k<4;k++)
+		{
+		  if(connect[k] > 0 && connect[k] < clabel)
+		    {
+		      temp.insert(connect[k]-1);// off by one because we assign colors starting at 1
+		      clabel = connect[k];
+		    }
+		  connect[k] = -1;
+		}
+	      
+	      if( clabel < INT_MAX ) // if we have a value use it
+		{
+		  setPixel(img,i,j,clabel);
+		  set<int>::iterator iter;
+		  for(iter = temp.begin(); iter!=temp.end(); ++iter)
+		    {
+		      labels[*iter].insert(temp.begin(),temp.end());
+		    }
+		}
+	      else // assign new 
+		{
+		  cnt++;
+		  setPixel(img,i,j,cnt);
+		  set<int> newSet;
+		  newSet.insert(cnt);
+		  labels.push_back(newSet);
+		}
+	    }
+	}
+    }
+  vector< set<int> >::iterator iter;
+  set<int>::iterator target;
+  int idx = 0;
+  cout << "DERP" << labels.size() << endl;
+  for(iter = labels.begin(); iter != labels.end();++iter)
+    {
+      cout << idx << " : ";
+      for(target = iter->begin();target!=iter->end();++target)
+	{
+	  cout << *target << " ";
+	}
+      cout << endl;
+      idx++;
+    }
+  //    for( target = (labels.begin()++); target !=labels.end();)
+  //	{
+  //	}
+  //  }
   return retVal;
 }
 /******************************************************************************************/ 
