4cc113fa68e2e4cdf81f8475b8a28cfbbcb22052
Katherine Scott <katherineAScott@gmail.com>
2011-10-03 23:39:25 -0400

    a little further along

---
+++ hw3/p2.cpp
@@ -0,0 +1,53 @@
+#include "vision_utilities.h"
+#include "vision_methods.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+  if(argc != 4 )
+    {
+      cout << "Argument error" << endl;
+      return 1;
+    }
+  char * inname = argv[1];
+
+  Image * inImg = new Image();
+  int retVal = readImage(inImg,inname);
+  
+  if( retVal || inImg == NULL )
+    {
+      cout << "Bad input image name:" << inname << endl;
+      return retVal;
+    }
+  std::string dbName(argv[2]);
+ 
+
+  std::vector<SObjectLabel> data;
+  data = get_morphology(inImg);
+  std::vector<SObjectLabel>::iterator iter;
+  for( iter = data.begin(); iter != data.end(); ++iter)
+    {
+      apply_label(inImg,*iter);
+    }
+  retVal = write_database(dbName,data);
+  if( retVal )
+    {
+      cout << "Could not write output database" << endl;
+      return retVal;
+    }
+  
+  char * oname = argv[3];
+  retVal = writeImage(inImg,oname);
+  if( retVal )
+    {
+      cout << "Error saving file: " << oname << endl;
+      return retVal;
+    }
+  return 0;
+}
