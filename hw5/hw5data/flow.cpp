b974ebbc588eb3e516e50150dc94c36197415de6
Katherine Scott <katherineAScott@gmail.com>
2011-11-20 14:07:35 -0500

    added makefile

--- hw5/hw5data/flow.cpp
+++ hw5/hw5data/flow.cpp
@@ -16,7 +16,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-  if(argc != 3 )
+  if(argc != 5 )
     {
       cout << "Argument error" << endl;
       return 1;
@@ -25,24 +25,13 @@ int main(int argc, char *argv[])
 
   Image * inImg = new Image();
   int retVal = readImage(inImg,inname);
-  // threshold 
+    // threshold 
   threshold(inImg,5);
-  // do a hacked connected components that spits out the result. 
+  /* // do a hacked connected components that spits out the result. 
   int count = connect_components(inImg);
   vector<SObjectLabel> labels; 
   labels = get_morphology(inImg);
   vector<SObjectLabel>::iterator iter;
-  for( iter = labels.begin(); iter != labels.end(); ++iter)
-    {
-      //      cout << iter->m_maxX << " " << iter->m_minX << " "
-      //  << iter->m_maxY << " " << iter->m_minY << endl;
-
-      float a = (float)iter->m_area;
-      //cout << (iter->m_maxX - iter->m_minX)/2.00 << " " << (iter->m_maxY - iter->m_minY)/2.00 << endl;
-      //cout << sqrt((a/3.1415962));
-    }
-
-
   char * oname = argv[2];
   ofstream myFile;
   myFile.open(oname);
@@ -65,7 +54,7 @@ int main(int argc, char *argv[])
       cout << "Error saving file: " << oname << endl;
       return retVal;
     }
-  cleanup(inImg);
+  cleanup(inImg);*/
   // should clean up
   return 0;
 }
